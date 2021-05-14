a8e974f95010b6a3b960e03300d59c4e7eb3a87d
Bao02Harry <82230028+Bao02Harry@users.noreply.github.com>
2021-05-14 10:25:43 +0700

    Update Header file

--- Header.h
+++ Header.h
@@ -1,29 +1,37 @@
 #ifndef Header_h
 #define Header_h
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 #include <string>
 #include <cmath>
 #include <time.h>
+#include <sstream>
 using namespace std;
 struct Teacher{
-    int* No;// so thu tu
-    int* TeID;
+    int No;// so thu tu
+    int TeID;
     string Fname;
     string Lname;
     string Gen; 
-    int* SocialID; // CMND
+    int SocialID; // CMND
+    string pass;
 };
 struct Student{
-    int* No; 
-    int* StuID;
+    int No; 
+    int StuID;
     string Fname;
     string Lname;
     string Gen; 
     string Date;
-    int* SocialID;
+    int SocialID;
+    string Class;
+    string Pass;
 };
+struct Cours{
 
+};
+// Doc file CSV va luu vao cau truc 
 void Color(int i );
 
 
